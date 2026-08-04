4b16821a6b078c1a94ae5f7c99d09bfc9e5952b1
njengajn <infonjenga@gmail.com>
2026-08-05 00:47:03 +0100

    bypass static for resting.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -23,10 +23,21 @@ urlpatterns += static(
 )
 
 
+# Media (TEMPORARY TEST)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
+
+
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns += [
     re_path(
         r"^media/(?P<path>.*)$",
         serve,
         {"document_root": settings.MEDIA_ROOT},
+        name="media",
     ),
 ]
